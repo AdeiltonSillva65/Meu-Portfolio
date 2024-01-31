3c779a55efa6e854f5668b5e15269d2916e9e06d
Adeilton Francisco da Silva <adeiltonsillva65@gmail.com>
2024-01-31 12:53:19 -0300

    imagem posicionada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Menu from "@/components/menu"
+import Sobre from "@/components/sobre";
 
 const page = () => {
   return (
     <section className="h-screen bg-zinc-900">
       <Menu />
+      <Sobre />
     </section >
   );
 }
--- src/components/sobre.tsx
+++ src/components/sobre.tsx
@@ -1,6 +1,26 @@
+import Image from "next/image";
+import Eu from '../../public/portifolio.png'
 const sobre = () => {
     return (
-        <h1>neymar</h1>
+        <div className=" m-16 h-4/5 bg-slate-900 flex   ">
+            <div className=" w-2/4 h-ful flex justify-center items-center">
+                <Image
+                    src={Eu}
+                    width={500}
+                    height={500}
+                    alt={""}
+                />
+            </div>
+
+            <div className="text-white flex-col text-justify w-2/4">
+                <p className="pt-16">Sobre</p>
+                <h1>Adeilton Francisco da Silva</h1>
+                <p>Desenvolvedor Front-End</p>
+                <p>Ola eu sou um Desenvolvedor band emd </p>
+                <button className="border">Dowloand CV</button>
+
+            </div>
+        </div>
     );
 }
 
